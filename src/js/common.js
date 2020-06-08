139af1caeaf0e3f6408db63e00364be7fe24b81d
winde <winde100kg@gmail.com>
2020-06-08 14:18:49 +0300

    added several sections

--- src/js/common.js
+++ src/js/common.js
@@ -17,5 +17,99 @@ $(document).ready(function() {
 		}, 200);
 	});
 
+	// Sliders
+	var promoSlider = new Swiper('.js-promo-slider', {
+		loop: true,
+		spaceBetween: 0,
+		pagination: {
+			el: '.js-promo-slider-pagination',
+			clickable: true
+		},
+		autoplay: {
+			delay: 8000,
+		},
+	});
+
+	$('.js-car-card-slider').each(function() {
+		var self = $(this);
+		var wrapper = self.parents('.car-card__slider');
+
+		var carCardSlider = new Swiper(self, {
+			loop: true,
+			spaceBetween: 0,
+			pagination: {
+				el: wrapper.find('.js-car-card-slider-pagination'),
+				clickable: true
+			},
+			navigation: {
+				prevEl: wrapper.find('.js-car-card-slider-btn-prev'),
+				nextEl: wrapper.find('.js-car-card-slider-btn-next'),
+			},
+			touchRatio: 0
+			// autoplay: {
+			// 	delay: 8000,
+			// },
+		});
+	});
+
+
+	var stocksSlider = new Swiper('.js-stocks-slider', {
+		slidesPerView: 3,
+		spaceBetween: 0,
+		navigation: {
+			prevEl: '.js-stocks-slider-btn-prev',
+			nextEl: '.js-stocks-slider-btn-next',
+		}
+	});
+
+	// Fancybox
+	$('[data-fancybox]').fancybox({
+		loop: true,
+		buttons: ['close']
+	});
+
+	// Tabs
+	$('.js-tab-content').not(":first").hide();
+	$('.js-tab-btn:first').addClass('is-active');
+
+	$('.js-tab-btn').on('click', function(e) {
+		e.preventDefault();
+		$('.js-tab-content').removeClass('is-active');
+		$('.js-tab-btn').removeClass('is-active').eq($(this).index()).addClass('is-active');
+		$('.js-tab-content').hide().eq($(this).index()).fadeIn().addClass('is-active');
+	}).eq(0).addClass('is-active');
+
+
+	// Custom inputs
+	var nonLinearSlider = $('price');
+
+	if (nonLinearSlider) {
+
+		noUiSlider.create(nonLinearSlider, {
+			connect: true,
+			behaviour: 'tap',
+			start: [0, 1000000],
+			range: {
+				'min': [0],
+				'max': [1000000]
+			}
+		});
+
+		var nodes = [
+			document.getElementById('priceLow'), // 0
+			document.getElementById('priceHigh')  // 1
+		];
+
+		var priceInput = $('.js-price-input');
+
+		nonLinearSlider.noUiSlider.on('update', function (values, handle, unencoded, isTap, positions) {
+			nodes[handle].innerHTML = values[handle];
+			priceInput.val(values[handle]);
+		});
+	}
+
+
+	$('input[type=tel]').mask('+7 (999) 999-99-99');
+
 
 });
